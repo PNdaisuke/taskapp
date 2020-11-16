45ae910a8f15970bda14ee32182208c2fd2dbd1d
daisuke.tsuruta <dtsuruta@planetnine.co.jp>
2020-11-16 19:04:02 +0900

    Initial Commit

---
+++ taskapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  taskapp
+//
+//  Created by Daisuke Tsuruta on 2020/11/16.
+//  Copyright © 2020 daisuke.tsuruta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
